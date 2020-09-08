2567c78e1829cfd75c7f257f81e564787ee0b2fb
kento28 <atnorisiro@gmail.com>
2020-09-08 15:47:25 +0900

    credential

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../checked")
 require("../memo")
-console.log("application2")
+console.log("application3")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
