b05c30509acbb63ddd78f32cf40e03ba370f7a68
kento28 <atnorisiro@gmail.com>
2020-09-08 15:29:57 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("./checked")
-require("./memo")
-console.log("application")
+require("../checked")
+require("../memo")
+console.log("application2")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
